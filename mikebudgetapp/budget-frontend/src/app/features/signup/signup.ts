c96484b25e46d52e4b475d1f7ca6df91c89e2125
Michael Casella <mjc52618@uga.edu>
2025-07-21 14:41:35 -0400

    These changes also don't break the repo

--- mikebudgetapp/budget-frontend/src/app/features/signup/signup.ts
+++ mikebudgetapp/budget-frontend/src/app/features/signup/signup.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
