cabd11dac21191debb22716f324e00a578bec611
Michael Casella <mjc52618@uga.edu>
2025-07-21 14:44:47 -0400

    These changes don't break

---
+++ mikebudgetapp/budget-frontend/src/app/core/services/userservice.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../../shared/models/user';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  signUpUser(user: User): Observable<void> {
+    return this.http.post<void>('http://localhost:9000/users', user);
+  }
+
+  logInAUser(user: User): Observable<void> {
+    return this.http.post<void>('http://localhost:9000/login', user);
+  }
+
+  getAUserById(userId: number): Observable<User> {
+    return this.http.get<User>(`http://localhost:9000/getUserById?userId=${userId}`);
+  }
+
+  updateUser(user: {username: string}): Observable<User> {
+    return this.http.put<User>('http://localhost:9000/updateUserById', user);
+  }
+
+}
