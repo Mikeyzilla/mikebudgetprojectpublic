0da2caec384cd7f24d5fbf54956fe8d5caf5cb43
Michael Casella <mjc52618@uga.edu>
2025-07-25 14:32:43 -0400

    If logged in, signup and log in should now be hidden in the nav bar.

--- mikebudgetapp/KnownIssues.md
+++ mikebudgetapp/KnownIssues.md
@@ -4,9 +4,9 @@ A) Style export budget.
 
 B) Separate CSS concerns - Angular supports multi-css files impacting a single HTML file.
 
-C) Since we can track logged in status using user session service, inject it into the nav bar, and hide sign up & log in buttons if the user is already logged in. 
+C) See if there's a way to, where if the user is not logged in and both buttons are visible, change the CSS of the Nav Bar Slogan.
 
-D) See if there's a way to, where if the user is not logged in and both buttons are visible, change the CSS of the Nav Bar Slogan.
+D) Add a logout button to the nav bar if and only if logged in.
 
 E) Test login, submit budget, and export budget to make sure it works in reality (use the server alongside the frontend).
 
--- mikebudgetapp/budget-frontend/src/app/components/navigation-bar/navigation-bar.ts
+++ mikebudgetapp/budget-frontend/src/app/components/navigation-bar/navigation-bar.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { UserSessionService } from '../../core/services/user-session-service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -10,9 +11,18 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './navigation-bar.scss'
 })
 export class NavigationBar {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userSessionService: UserSessionService) {}
 
   get currentRoute(): string {
     return this.router.url;
   }
+
+  checkIfLoggedIn(): boolean {
+    let isLoggedIn = false;
+    if (this.userSessionService.getOurUserId() != 0) {
+      isLoggedIn = true;
+    }
+    return isLoggedIn;
+  }
+
 }
